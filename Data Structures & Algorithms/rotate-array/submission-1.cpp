8e5a21e5712ca3e2764ffe8c538944dc658af976
Shivi Tiwari <148358444+shivi028@users.noreply.github.com>
2026-08-02 23:45:00 +0530

    Add: rotate-array - submission-1

---
+++ Data Structures & Algorithms/rotate-array/submission-1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        if(k == 0) return;
+        k = k % nums.size();
+        if(k == 0) return;
+
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin()+k);
+        reverse(nums.begin()+k, nums.end());
+        
+
+    }
+};
