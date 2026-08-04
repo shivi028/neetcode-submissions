205572f1989ddbc196d0d3b3d98a047b71b251f6
Shivi Tiwari <148358444+shivi028@users.noreply.github.com>
2026-08-04 18:03:54 +0530

    Add: find-k-closest-elements - submission-0

---
+++ Data Structures & Algorithms/find-k-closest-elements/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        // binary search
+        vector<int>result;
+        int s = 0, e = arr.size()-1;
+        while(e-s >= k){
+            if( abs(arr[s]-x) > abs(arr[e]-x) ){
+                s++;
+            }
+                else e--;
+        }
+        return vector<int>(arr.begin()+s, arr.begin()+e+1);
+    }
+        
+
+    
+};
